03d7e16c8c38b7d64c9293ce52f6175c263d7cbd
Dave Küppers <dave@evad.nl>
2026-03-21 22:14:31 +0100

    add hyprland.nix config

---
+++ modules/hyprland.nix
@@ -0,0 +1,26 @@
+{ config, lib, pkgs, ... }:
+
+{
+  programs.hyprland.enable = true;
+
+  # Sound settings
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
+
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}:/bin/tuigreet --time --cmd Hyprland";
+        user = "greeter";
+      };
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    kitty
+  ]
+
+}
