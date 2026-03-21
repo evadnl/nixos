fc9e75231c168ddf8aeb139d9fed001d88d8c4c9
Dave Küppers <dave@evad.nl>
2026-03-21 23:29:02 +0100

    Add common settings module

---
+++ modules/common.nix
@@ -0,0 +1,23 @@
+{ config, lib, pkgs, ... }:
+
+{
+  time.timeZone = "Europe/Amsterdam";
+
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    git
+    curl
+    htop
+    iotop
+    neofetch
+  ];
+
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+   keyMap = "us";
+  };
+
+  # Dynamic linker compatibility
+  programs.nix-ld.enable = true;
+}
