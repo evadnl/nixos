a22d2f2130e690dbca12894ca53d1e5bd55687b9
Dave Küppers <dave@evad.nl>
2026-03-23 19:48:35 +0100

    Pre-add home-manager for future

--- flake.nix
+++ flake.nix
@@ -3,6 +3,8 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # home-manager.url = "github:nix-community/home-manager";
+    # home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, ... }: {
