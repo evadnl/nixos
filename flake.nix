53ee1ad6352ed5a73b8f768b08182143cfefa842
Dave Küppers <dave@evad.nl>
2026-03-25 20:36:00 +0100

    Add Lanzaboot for secureboot

--- flake.nix
+++ flake.nix
@@ -3,37 +3,53 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
     disko = {
       url = "github:nix-community/disko/latest";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    lanzaboote = {
+      url = "github:nix-community/lanzaboote/v0.4.2";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     # home-manager = {
     #   url = "github:nix-community/home-manager";
     #   inputs.nixpkgs.follows = "nixpkgs";
     # };
   };
 
-  outputs = { self, nixpkgs, disko, ...}: {
-    nixosConfigurations.nixos-vm = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./hosts/nixos-vm/configuration.nix
-        ./modules/hyprland.nix
-        ./modules/common.nix
-      ];
-    };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      disko,
+      lanzaboote,
+      ...
+    }:
+    {
+      nixosConfigurations.nixos-vm = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/nixos-vm/configuration.nix
+          ./modules/hyprland.nix
+          ./modules/common.nix
+        ];
+      };
 
-    nixosConfigurations.ares = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        disko.nixosModules.disko
-        ./hosts/ares/disk.nix
-        ./hosts/ares/configuration.nix
-        ./hosts/ares/hardware-configuration.nix
-        ./modules/amd-cpu.nix
-        ./modules/nvidia.nix
-        ./modules/locale.nix
-      ];
+      nixosConfigurations.ares = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          disko.nixosModules.disko
+          lanzaboote.nixosModules.lanzaboote
+          ./hosts/ares/disk.nix
+          ./hosts/ares/configuration.nix
+          ./hosts/ares/hardware-configuration.nix
+          ./modules/amd-cpu.nix
+          ./modules/nvidia.nix
+          ./modules/locale.nix
+        ];
+      };
     };
-  };
 }
