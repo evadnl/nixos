452f4a487e5476af94135cf87c81e4250bf7ebdd
Dave Küppers <dave@evad.nl>
2026-03-31 21:37:07 +0200

    feat: add Spotify

--- hosts/ares/home-configuration.nix
+++ hosts/ares/home-configuration.nix
@@ -9,6 +9,7 @@
     ../../modules/home/services/dunst.nix
     ../../modules/home/apps/terminals/ghostty.nix
     ../../modules/home/apps/social/discord.nix
+    ../../modules/home/apps/media/spotify.nix
     ../../modules/home/apps/dev/claude-code.nix
     ../../modules/home/apps/browsers/firefox.nix
     ../../modules/home/apps/editors/vscode.nix
