764adb559d2e7d3c0b0652abe7d12e6001e76507
Dave Küppers <dave@evad.nl>
2026-03-31 15:46:46 +0200

    feat: set Firefox dark mode and home.evad.nl as start page

--- modules/home/apps/browsers/firefox.nix
+++ modules/home/apps/browsers/firefox.nix
@@ -1,5 +1,14 @@
 { ... }:
 
 {
-  programs.firefox.enable = true;
+  programs.firefox = {
+    enable = true;
+    profiles.default = {
+      settings = {
+        "browser.startup.homepage" = "https://home.evad.nl";
+        "ui.systemUsesDarkTheme" = 1;
+        "browser.in-content.dark-mode.enabled" = true;
+      };
+    };
+  };
 }
