d461bd9bcb04c8f84cd1a537728a882b950f2239
Dave Küppers <dave@evad.nl>
2026-04-03 14:28:52 +0200

    feat: add dconf dark mode preference for GTK dialogs

--- modules/home/desktop/gtk.nix
+++ modules/home/desktop/gtk.nix
@@ -6,4 +6,8 @@
     gtk3.extraConfig.gtk-application-prefer-dark-theme = true;
     gtk4.extraConfig.gtk-application-prefer-dark-theme = true;
   };
+
+  dconf.settings."org/gnome/desktop/interface" = {
+    color-scheme = "prefer-dark";
+  };
 }
