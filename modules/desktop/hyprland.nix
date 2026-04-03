fc30863df44ee180766a19e4275ed0538c977424
Dave Küppers <dave@evad.nl>
2026-04-03 14:28:30 +0200

    feat: changed theming for SDDM

--- modules/desktop/hyprland.nix
+++ modules/desktop/hyprland.nix
@@ -7,6 +7,17 @@
 
 let
   cfg = config.desktop.hyprland;
+
+  sddm-wallpaper = "/var/lib/sddm/wallpaper";
+
+  sddm-theme = pkgs.catppuccin-sddm.override {
+    flavor = "mocha";
+    accent = "mauve";
+    font = "JetBrainsMono Nerd Font Propo";
+    fontSize = "10";
+    background = sddm-wallpaper;
+    loginBackground = true;
+  };
 in
 {
   options.desktop.hyprland = {
@@ -28,26 +39,38 @@ in
       wireplumber.enable = true;
     };
 
-    environment.etc."issue".text = ''
-      ███▄▄▄▄    ▄█  ▀████     ████▀  ▄██████▄     ▄████████
-      ███▀▀▀██▄ ███    ███    ████▀  ███    ███   ███    ███
-      ███   ███ ███     ███   ███    ███    ███   ███    █▀
-      ███   ███ ███     ▀███▄███▀    ███    ███   ███
-      ███   ███ ███     ████▀██▄     ███    ███ ▀███████████
-      ███   ███ ███     ███  ▀███    ███    ███          ███
-      ███   ███ ███   ▄███     ███▄  ███    ███    ▄█    ███
-       ▀█   █▀  █▀   ████       ███▄  ▀██████▀   ▄████████▀
+    services.libinput.enable = true;
 
-                      Follow the white rabbit.
-    '';
-
-    services.greetd = {
-      enable = true;
-      settings = {
-        default_session = {
-          command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --asterisks --issue --greet-align left --cmd \"uwsm start hyprland-uwsm.desktop\"";
-          user = "greeter";
+    # --- SDDM display manager ---
+    services.displayManager = {
+      sddm = {
+        enable = true;
+        wayland.enable = true;
+        wayland.compositor = "kwin";
+        theme = "${sddm-theme}/share/sddm/themes/catppuccin-mocha-mauve";
+        package = pkgs.kdePackages.sddm;
+        settings.Theme = {
+          CursorTheme = "catppuccin-mocha-dark-cursors";
+          CursorSize = 24;
         };
+        extraPackages = [ pkgs.catppuccin-cursors.mochaDark ];
+      };
+      defaultSession = "hyprland-uwsm";
+    };
+
+    environment.systemPackages = [ sddm-theme ];
+
+    # Copy a random wallpaper to a system-wide path readable by SDDM
+    systemd.services.sddm-wallpaper-sync = {
+      description = "Pick random wallpaper for SDDM greeter";
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = pkgs.writeShellScript "sddm-wallpaper-sync" ''
+          dest="${sddm-wallpaper}"
+          wallpaper=$(find "/home/${config.user.name}/Pictures/wallpapers" -type f \( -name "*.jpg" -o -name "*.jpeg" -o -name "*.png" \) | shuf -n 1)
+          [ -n "$wallpaper" ] && cp "$wallpaper" "$dest" && chmod 644 "$dest"
+        '';
       };
     };
 
