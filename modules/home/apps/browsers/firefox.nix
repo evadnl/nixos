f9d778bda56e52456c62fdc75d56c968eaae8b68
Dave Küppers <dave@evad.nl>
2026-04-03 14:28:40 +0200

    feat: enable session restore in Firefox

--- modules/home/apps/browsers/firefox.nix
+++ modules/home/apps/browsers/firefox.nix
@@ -6,6 +6,8 @@
     profiles.default = {
       settings = {
         "browser.startup.homepage" = "https://home.evad.nl";
+        "browser.sessionstore.resume_from_crash" = true;
+        "browser.startup.page" = 3;
         "ui.systemUsesDarkTheme" = 1;
         "browser.in-content.dark-mode.enabled" = true;
       };
