bfe1116658f99a0660b0003f6cd697b782dbb574
Dave Küppers <dave@evad.nl>
2026-04-03 14:29:16 +0200

    feat: set LC_TIME to en_GB for 24h time format

--- modules/os/locale.nix
+++ modules/os/locale.nix
@@ -3,6 +3,7 @@
 {
   time.timeZone = "Europe/Amsterdam";
   i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings.LC_TIME = "en_GB.UTF-8";
 
   # TTY console
   console.keyMap = "us-acentos";
